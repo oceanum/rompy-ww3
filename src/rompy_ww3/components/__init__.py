ae9989fa0f76be6638413fc64b849bd169b8ec99
Tom Durrant <t.durrant@oceanum.science>
2025-10-22 09:13:49 +1100

    Renamed componet files

--- src/rompy_ww3/components/__init__.py
+++ src/rompy_ww3/components/__init__.py
@@ -1,16 +1,16 @@
 """Components package for WW3 configuration."""
 
 from .basemodel import WW3ComponentBaseModel
-from .shell import Shel
+from .shel import Shel
 from .grid import Grid
 from .multi import Multi
-from .boundary import Bound
-from .boundary_update import Bounc
-from .field_preprocessor import Prnc
-from .track import Trnc
-from .field_output import Ounf
-from .point_output import Ounp
-from .restart_update import Uptstr
+from .bound import Bound
+from .bounc import Bounc
+from .prnc import Prnc
+from .trnc import Trnc
+from .ounf import Ounf
+from .ounp import Ounp
+from .uptstr import Uptstr
 from .parameters import Parameters
 
 __all__ = [
