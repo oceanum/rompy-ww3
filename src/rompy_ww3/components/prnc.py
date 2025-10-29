2acd7ed9d50e6862bbb2c7b24ce21e5bfbb771a9
Tom Durrant <t.durrant@oceanum.science>
2025-10-29 14:01:56 +1100

    Added validator to set id in forcing dataset to write nc for meaningful name

--- src/rompy_ww3/components/prnc.py
+++ src/rompy_ww3/components/prnc.py
@@ -1,10 +1,11 @@
 """Field preprocessor component for WW3 configuration."""
 
 from typing import Optional
-from pydantic import Field
+from pydantic import Field, model_validator
 from ..namelists.forcing import Forcing
 from ..namelists.file import File
 from .basemodel import WW3ComponentBaseModel
+from ..core.data import WW3DataBlob, WW3DataGrid
 
 
 class Prnc(WW3ComponentBaseModel):
@@ -23,3 +24,10 @@ class Prnc(WW3ComponentBaseModel):
     @property
     def prepend_cmd(self) -> str:
         return f"ln -sf {self.nml_filename} ww3_prnc.nml"
+
+    @model_validator(mode="after")
+    def set_datasource_id(self):
+        """Ensure id are constent with WW3DataGrid"""
+        if isinstance(self.file.filename, (WW3DataBlob, WW3DataGrid)):
+            self.file.filename.id = self.forcing.ww3_var_name.lower()
+        return self
